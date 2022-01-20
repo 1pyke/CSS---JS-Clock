a1ac4e2d799fa0005d76e3bb653b5ffea3f19769
1pyke <liethzaitoun99@gmail.com>
2022-01-20 19:42:48 +0200

    fix hands issues

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-const hands = document.querySelector('.hand')
+const hands = document.querySelectorAll('.hand');
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand')
+const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + 90
+    const hoursDegrees = ((hours / 12) * 360) + 90;
     const secoundsDegrees = ((seconds / 60) * 360) + 90;
     const minDegress = ((minutes / 60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
-    minHand.style.transform = `rotate(${minDegress}deg)`
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    minHand.style.transform = `rotate(${minDegress}deg)`;
     secondHand.style.transform = `rotate(${secoundsDegrees}deg)`
     console.log(seconds);
     if (seconds === 0) {
-        // hands.forEeach(hand => hand.style.transitionDuration = '0s');
-        secondHand.style.transitionDuration = '0s';
-        minHand.style.transitionDuration = '0s';
-        hourHand.style.transitionDuration = '0s';
-
-
+        hands.forEach(hand => hand.style.transitionDuration = '0s');
     } else {
-        // hands.forEeach(hand => hand.style.transitionDuration = '0.05s');
-        secondHand.style.transitionDuration = '0.05s';
-        minHand.style.transitionDuration = '0.05s';
-        hourHand.style.transitionDuration = '0.05s';
+        hands.forEach(hand => hand.style.transitionDuration = '0.05s');
     }
 
 }
 setInterval(setDate, 1000);
+setDate();
